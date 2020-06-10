0e64687c2028ae7e49985b7cbec1296d326d75b2
francis1240 <yma@macalester.edu>
2020-06-10 17:14:44 -0500

    debug-publicurl

--- src/components/homeHeader.js
+++ src/components/homeHeader.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import githublogo from '../res/img/GitHub.png'
 
+const grow = {
+    flexGrow: 1
+};
+
 
 const homeHeader = () =>{
     return (
         <>
             <div style="display:flex;">
                 <div>Yufan Ma</div>
-                <div style="flex-grow:1;"></div>
+                <div style={grow}></div>
                 <img href="https://github.com/Francis1240/Homepage" src={githublogo} alt="GitHub repository for this page."/>
             </div>
         </>
